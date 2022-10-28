5b37d8b4334b1ea96a31daa53ba63030f470a900
Sirvelasque <velasquezwalter10@yahoo.es>
2022-10-28 11:28:04 -0600

    Fix menu's closing interaction

--- src/components/menus/mainMenu.js
+++ src/components/menus/mainMenu.js
@@ -4,21 +4,19 @@ import Filters from "../Filters";
 
 const MainMenu = ({toggleMenu}) => {
   const [show, setShow] = useState(false);
-  const toggleShow = (i) => {
-    if (i.name==="Catalogo" || i==="x") setShow(!show);
+  const toggleShow = () => {
+    setShow(!show);
   };
-  const toggleAll = (i) => {
-    if (i.name==="Catalogo" || i==="x") {
-      toggleShow(i);
+  const toggleAll = () => {
+      toggleShow();
       toggleMenu();
-    }
   }
   const menuItems=[
     {
       name:'Inicio',
       icon:'fa-solid fa-house',
       k:1,
-      task: toggleAll,
+      task: toggleMenu,
       link:"/",
     },
     {
@@ -32,21 +30,21 @@ const MainMenu = ({toggleMenu}) => {
       name:'Quienes somos',
       icon:'fa-solid fa-book-open-reader',
       k:3,
-      task: toggleAll,
+      task: toggleMenu,
       link:"/Detalles",
     },
     {
       name:'Contactanos',
       icon:'fa-solid fa-phone',
       k:4,
-      task: toggleAll,
+      task: toggleMenu,
       link:"/",
     },
   ];
 
   const arrows = () => (
     <div id="main_arrows" className="arrows">
-    {menuItems.map((i)=> (<div onClick={ () => toggleShow(i) }>{'>'}</div>))}
+    {menuItems.map((i)=> (<div onClick={ (e) => toggleMenu(e) }>{'>'}</div>))}
     </div>
   );
 
@@ -54,17 +52,17 @@ const MainMenu = ({toggleMenu}) => {
     <div id="main_menu" className='list_container'>
       <ul id="main_menu_list" className="filters">
         {menuItems.map((i)=>(
-          <li className="filter_btn" key={i.k} onClick={ () => toggleShow(i) }>
             <NavLink to={i.link}>
+          <li className="filter_btn" key={i.k} onClick={ (e) => i.task(e) }>
               <i className={i.icon} > </i> {i.name} 
-            </NavLink>
           </li>
+            </NavLink>
           ))}
       </ul>
       {arrows()}
 
       <div id="Catalogue" className={show ? "show" : "hiden-r"}>
-      <div className="exit" onClick={ () => toggleAll("x") }>
+      <div className="exit" onClick={ () => toggleAll() }>
         <i className="fa-solid fa-x"></i>
       </div>
         <Filters />
